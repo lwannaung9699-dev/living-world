915072bfd2d0f392d2950ae85d1ed3d00b2b1644
lwannaung9699-dev <lwannaung9699@gmail.com>
2026-08-20 13:01:20 +0630

    Remove stray duplicated living-world-team08-reconciled/ folder from repo root

--- living-world-team08-reconciled/src/sim/worldgen/errors.ts
+++
@@ -1,4 +0,0 @@
-import { SimulationError } from "../core/errors";
-
-/** Thrown when worldgen field/chunk queries are attempted against a WorldState that hasn't run generateWorldGenesis() yet. */
-export class WorldGenesisNotInitializedError extends SimulationError {}
